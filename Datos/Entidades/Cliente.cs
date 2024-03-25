96e7ab334b10c1879165d3ac15d1a851a4694cb4
Alexander Galan <Alexander Galan@DESKTOP-Q6JBD0N>
2024-03-24 21:38:51 -0600

    Agregar archivos de proyecto.

---
+++ Datos/Entidades/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.Entidades
+{
+    internal class Cliente
+    {
+        public int ClienteId { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public bool Estado { get; set; }
+    }
+}
---
+++ Datos/Entidades/Renta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.Entidades
+{
+    internal class Renta
+    {
+        public int RentaId { get; set; }
+        public int ClienteId { get; set; }
+        public int PeliculaId { get; set; }
+        public DateTime FechaRenta { get; set; }
+        public DateTime FechaRetorno { get; set; }
+        public int Cantidad { get; set; }
+        public decimal PrecioRenta { get; set; }
+    }
+}
